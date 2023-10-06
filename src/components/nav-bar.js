cdcda28cc5f412bf454abf2e0d2d8c1c1fa65690
Alfonso <81327113+foncheto@users.noreply.github.com>
2023-10-06 20:13:02 -0300

    Update nav-bar.js

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar-container"> {/* Agrega la clase navbar-container aquí */}
+    <div className="navbar-container">
+      {" "}
+      {/* Agrega la clase navbar-container aquí */}
       <nav className="navbar">
         <ul className="navbar-list">
-          <li className="navbar-item">
-            <Link to="/" className="navbar-link"><p>Inicio</p></Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/calculo" className="navbar-link"><p>Cálculos</p></Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/contacto" className="navbar-link"><p>Contacto</p></Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/noabrir" className="navbar-link"><p>No Abrir</p></Link>
-          </li>
+          <Link to="/" className="navbar-link">
+            <li className="navbar-item">
+              <p>Inicio</p>
+            </li>
+          </Link>
+          <Link to="/calculo" className="navbar-link">
+            <li className="navbar-item">
+              <p>Cálculos</p>
+            </li>
+          </Link>
+          <Link to="/contacto" className="navbar-link">
+            <li className="navbar-item">
+              <p>Contacto</p>
+            </li>
+          </Link>
+          <Link to="/noabrir" className="navbar-link">
+            <li className="navbar-item">
+              <p>No Abrir</p>
+            </li>
+          </Link>
         </ul>
       </nav>
     </div>
