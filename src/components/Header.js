5eda6137642fe5f14a2b6b1d09d031b86165e882
Alfonso <81327113+foncheto@users.noreply.github.com>
2023-10-06 19:54:52 -0300

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Navbar from './nav-bar';
+import React from "react";
+import Navbar from "./nav-bar";
 
 const Header = () => {
   return (
     <header>
       <div className="hero-text">
-        <div className='solopresentacion'>
-          <h1>Bienvenido a la página de los Photoshopers</h1>
+        <div className="solopresentacion">
+          <h1>Los Photoshopers</h1>
         </div>
-        <div className='solonavbar'>
+        <div className="solonavbar">
           <Navbar /> {}
         </div>
       </div>
