6bf878f430e75ebf9ebad55589c81052a9e957bc
Alfonso <81327113+foncheto@users.noreply.github.com>
2023-10-06 20:05:20 -0300

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./nav-bar";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <div className="hero-text">
         <div className="solopresentacion">
-          <h1>Los Photoshopers</h1>
+          <Link to="/">
+            <h1>Los Photoshopers</h1>
+          </Link>
         </div>
         <div className="solonavbar">
           <Navbar /> {}
