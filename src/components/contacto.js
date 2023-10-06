1854684ae6da449558b42edb967bb541ae04efdb
Alfonso <81327113+foncheto@users.noreply.github.com>
2023-10-06 20:21:48 -0300

    update casi final

--- src/components/contacto.js
+++ src/components/contacto.js
@@ -1,47 +1,78 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Contacto = () => {
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowAlert(true);
+  };
+
   return (
-    <div className='formulario-container'>
-      <p>En caso que quieras comunicarte con alguien de forma personal, puedes presionar en la parte final de la página los links disponibles que tenemos para cada uno de los integrantes presentes</p>
-      <form className="containerformulario">
-      <fieldset>
-        <legend>Completa el formulario</legend>
-
-        <div className="contenerdor12">
-          <label htmlFor="name" className="form-label">
-            Nombre
-          </label>
-          <input type="text" className="form-control" id="name" placeholder="Nombre" />
+    <div className="formulario-container">
+      {showAlert && (
+        <div className="alert alert-success" role="alert">
+          ¡Formulario enviado con éxito!
         </div>
+      )}
+      <p>
+        En caso que quieras comunicarte con alguien de forma personal, puedes
+        presionar en la parte final de la página los links disponibles que
+        tenemos para cada uno de los integrantes presentes
+      </p>
+      <form className="containerformulario" onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Completa el formulario</legend>
 
-        <div className="contenedor12">
-          <label htmlFor="phone" className="form-label">
-            Teléfono
-          </label>
-          <input type="number" className="form-control" id="phone" placeholder="99999999" />
-        </div>
+          <div className="contenerdor12">
+            <label htmlFor="name" className="form-label">
+              Nombre
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              placeholder="Nombre"
+            />
+          </div>
 
-        <div className="contenedor12">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input type="email" className="form-control" id="email" placeholder="example@example.com" />
-        </div>
+          <div className="contenedor12">
+            <label htmlFor="phone" className="form-label">
+              Teléfono
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="phone"
+              placeholder="99999999"
+            />
+          </div>
 
-        <div className="contenedor12">
-          <label htmlFor="message" className="form-label">
-            Mensaje
-          </label>
-          <textarea className="form-control" id="message" rows="2"></textarea>
-        </div>
+          <div className="contenedor12">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="example@example.com"
+            />
+          </div>
 
-        <button type="submit" className="btn btn-primary">
-          Enviar
-        </button>
-      </fieldset>
-    </form>
-  </div>
+          <div className="contenedor12">
+            <label htmlFor="message" className="form-label">
+              Mensaje
+            </label>
+            <textarea className="form-control" id="message" rows="2"></textarea>
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Enviar
+          </button>
+        </fieldset>
+      </form>
+    </div>
   );
 };
 
